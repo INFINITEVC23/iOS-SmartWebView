5476f59bf5c8b8478d48658a50079b5ba13c0341
INFINITEVC23 <andreamateo1990@gmail.com>
2026-03-22 19:28:58 +0100

    Update LocationPlugin.swift

--- Plugins/LocationPlugin.swift
+++ Plugins/LocationPlugin.swift
@@ -2,10 +2,8 @@ import Foundation
 import CoreLocation
 
 class LocationPlugin: NSObject, CLLocationManagerDelegate {
-    // FIXED: Added shared instance
     static let shared = LocationPlugin()
     
-    // FIXED: Added register method to satisfy the App's registerPlugins call
     static func register() {
         print("Location Plugin Registered")
     }
@@ -15,14 +13,11 @@ class LocationPlugin: NSObject, CLLocationManagerDelegate {
     override init() {
         super.init()
         locationManager.delegate = self
-        // Standard accuracy for web-based location services
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
-    // Your original logic
     func requestInitialPermission() {
         let status = locationManager.authorizationStatus
-        
         if status == .notDetermined {
             locationManager.requestWhenInUseAuthorization()
         } else if status == .authorizedWhenInUse || status == .authorizedAlways {
@@ -30,21 +25,16 @@ class LocationPlugin: NSObject, CLLocationManagerDelegate {
         }
     }
     
-    // Delegate method to handle the user's response to the popup
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus {
         case .authorizedWhenInUse, .authorizedAlways:
-            print("Location Access Granted")
             locationManager.startUpdatingLocation()
-        case .denied, .restricted:
-            print("Location Access Denied")
         default:
             break
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // Your original logic
         guard let location = locations.last else { return }
         print("Lat: \(location.coordinate.latitude), Lon: \(location.coordinate.longitude)")
     }
