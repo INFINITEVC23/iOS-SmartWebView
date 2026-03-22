dd2db31c43ac0662a6a3e5883c972c3e5348c460
INFINITEVC23 <andreamateo1990@gmail.com>
2026-03-22 14:05:44 +0100

    Update PermissionManager.swift

--- Managers/PermissionManager.swift
+++ Managers/PermissionManager.swift
@@ -3,17 +3,13 @@ import UserNotifications
 import CoreLocation
 import UIKit
 
-// Manages all native permission requests for the application.
-class PermissionManager: NSObject { // Add CLLocationManagerDelegate
-    
-    // Use a singleton pattern to be accessible from anywhere and manage delegate callbacks.
+class PermissionManager: NSObject { 
     static let shared = PermissionManager()
     
     private override init() {
         super.init()
     }
     
-    // Central function to request all permissions listed in swv.properties on launch.
     func requestInitialPermissions() {
         let context = SWVContext.shared
         
@@ -22,22 +18,16 @@ class PermissionManager: NSObject { // Add CLLocationManagerDelegate
         }
         
         if context.permissionsOnLaunch.contains("LOCATION") {
-            // We just need to trigger the LocationPlugin to ask.
-            // A simple way is to instantiate its manager so it can request.
             if let locationPlugin = PluginManager.shared.getPlugin(named: "Location") as? LocationPlugin {
-                print("Triggering initial location permission request via LocationPlugin.")
                 locationPlugin.requestInitialPermission()
             }
         }
     }
     
-    // --- Notification Permissions ---
     private func requestNotificationPermission() {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
             DispatchQueue.main.async {
                 if granted {
-                    print("Notification permission granted.")
-                    // Once permission is granted, register for remote notifications.
                     UIApplication.shared.registerForRemoteNotifications()
                 } else if let error = error {
                     print("Notification permission error: \(error.localizedDescription)")
@@ -46,9 +36,6 @@ class PermissionManager: NSObject { // Add CLLocationManagerDelegate
         }
     }
 
-    // --- Delegate Method ---
-    // This delegate method is now handled here, but it doesn't need to do anything
-    // as the LocationPlugin will check the status again when it's used.
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         print("PermissionManager: Location authorization status changed to: \(manager.authorizationStatus.rawValue)")
     }
