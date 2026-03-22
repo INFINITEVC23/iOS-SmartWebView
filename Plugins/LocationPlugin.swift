299de4d657e6667dcd48a0f830a1353d5c0737c3
INFINITEVC23 <andreamateo1990@gmail.com>
2026-03-22 19:32:28 +0100

    Update LocationPlugin.swift

--- Plugins/LocationPlugin.swift
+++ Plugins/LocationPlugin.swift
@@ -3,39 +3,24 @@ import CoreLocation
 
 class LocationPlugin: NSObject, CLLocationManagerDelegate {
     static let shared = LocationPlugin()
-    
-    static func register() {
-        print("Location Plugin Registered")
-    }
+    static func register() { print("Location Ready") }
     
     private let locationManager = CLLocationManager()
     
-    override init() {
-        super.init()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    }
-    
     func requestInitialPermission() {
-        let status = locationManager.authorizationStatus
-        if status == .notDetermined {
-            locationManager.requestWhenInUseAuthorization()
-        } else if status == .authorizedWhenInUse || status == .authorizedAlways {
-            locationManager.startUpdatingLocation()
-        }
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
     }
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        switch manager.authorizationStatus {
-        case .authorizedWhenInUse, .authorizedAlways:
+        if manager.authorizationStatus == .authorizedWhenInUse {
             locationManager.startUpdatingLocation()
-        default:
-            break
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        print("Lat: \(location.coordinate.latitude), Lon: \(location.coordinate.longitude)")
+        if let loc = locations.last {
+            print("Location: \(loc.coordinate.latitude), \(loc.coordinate.longitude)")
+        }
     }
 }
